81f23e609dbe6ad159d0f80c957b55546ee1891c
tshepomaredi <tmaredi007@gmail.com>
2025-06-13 02:19:38 +0200

    build fail fix

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+// pages/about.tsx
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About - GnuScan</title>
+        <meta name="description" content="About GnuScan website security monitoring tool" />
+      </Head>
+
+      
+    </>
+  );
+}
